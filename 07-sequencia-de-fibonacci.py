253ea38f0274e35e84f3b5eaea889bd6b6d5358d
DanielSR1 <danielsrcontato@gmail.com>
2022-03-12 18:02:24 -0300

    sequencia de fibonacci

---
+++ 07-sequencia-de-fibonacci.py
@@ -0,0 +1,13 @@
+print('\033[4mVAMOS CALCULAR A SEQUENCIA DE FIBONACCI.\033[m')
+n=int(input('\033[32mQUANTOS TERMOS VOCÊ QUER MOSTRAR? '))
+t1=0
+t2=1
+print(f'{t1} {t2}', end=' ')
+cont=3
+while cont<=n:
+    t3=t1+t2
+    print(f'{t3}', end=' ')
+    cont=cont+1
+    t1=t2
+    t2=t3
+print('\033[35mFIM')
